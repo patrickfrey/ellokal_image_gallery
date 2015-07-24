aecb292494f5476e958095eebe892f030b55d047
patrickfrey <patrickpfrey@yahoo.com>
2015-07-24 10:48:44 +0200

    insert picture exif inf into database (work in progress)

--- docker/insertPictures.sh
+++ docker/insertPictures.sh
@@ -34,31 +34,22 @@ processImage() {
 
 	echo "INSERT INTO Concert (date,title) SELECT '$CONCERTDATE', '$CONCERTNAME' WHERE NOT EXISTS (SELECT * FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME');"
 	echo "INSERT INTO ConcertPicture (concertId,dim_X,dim_Y,brennweite,blende,verschlusszeit,date,program,resolution_X,resolution_Y,width,length,meta,fotograph,thumbnail,filename) SELECT id,'$DIM_X','$DIM_Y','$BRENNWEITE','$BLENDE','$EXPTIME','$DATETIME','$PROGRAM','$RESOLUTION_X','$RESOLUTION_Y','$WIDTH','$LENGTH','$META','$FOTOGRAPH','$THUMBNAIL','$FILENAME' FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME';"
-
-	# echo "THUMBNAIL=$THUMBNAIL"
-	# echo "DIM_X=$DIM_X"
-	# echo "DIM_Y=$DIM_Y"
-	# echo "BRENNWEITE=$BRENNWEITE"
-	# echo "BLENDE=$BLENDE"
-	# echo "EXPTIME=$EXPTIME"
-	# echo "DATETIME=$DATETIME"
-	# echo "PROGRAM=$PROGRAM"
-	# echo "RESOLUTION_X=$RESOLUTION_X"
-	# echo "RESOLUTION_Y=$RESOLUTION_Y"
-	# echo "WIDTH=$WIDTH"
-	# echo "LENGTH=$LENGTH"
-	# echo "META=$META"
-	# echo "FOTOGRAPH=$FOTOGRAPH"
 }
 
-# if [ -f "/srv/ellokal/.lastUpdate" ]; then
-# 	find /srv/ellokal/[0-9]* -name "*.jpg" -cnewer /srv/ellokal/.lastUpdate | xargs processImage
-# else
-# 	find /srv/ellokal/[0-9]* -name "*.jpg" | xargs processImage
-# fi
-# touch /srv/ellokal/.lastUpdate
+TIMSTMP="`date +'%y%m%d'`_`date +'%H%M%S'`"
+LOGFILE="/var/log/ellokal/insert_exif_$TIMSTMP.log"
+INPLIST="/var/log/ellokal/input_exif_$TIMSTMP.txt"
+LASTUPD="/srv/ellokal/.lastUpdate"
 
-processImage '/srv/ellokal/2014.08.25_howe gelb/D800E_040349.jpg' | psql -U strus -d ellokaldb -h localhost -f-
+if [ -f "$LASTUPD" ]; then
+	FINDOPT="-cnewer $LASTUPD"
+fi
+find /srv/ellokal/[0-9]* -name "*.jpg" $FINDOPT > $INPLIST
+while read filename <&3; do
+	processImage "$filename" | psql --log-file="$LOGFILE" -U strus -d ellokaldb -h localhost -f-
+done 3< $INPLIST
+touch "$LASTUPD"
+
 
 
 
