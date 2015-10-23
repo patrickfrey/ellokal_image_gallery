9d57d36523762c536057170fa997ebe3150f4503
patrickfrey <patrickpfrey@yahoo.com>
2015-10-23 16:40:09 +0200

    ...

--- scripts/insertPictures.sh
+++ scripts/insertPictures.sh
@@ -14,17 +14,15 @@ thumbnail() {
 processImage() {
 	FILENAME=`echo "$1" | sed 's@/data/ellokal.project-strus.net/@@'`
 	THUMBNAIL=`thumbnail "$1"`
-	DIM_X=`imageTag "$1" "Pixel X Dimension"`
-	DIM_Y=`imageTag "$1" "Pixel Y Dimension"`
+	WIDTH=`imageTag "$1" "Exif Image Width"`
+	LENGTH=`imageTag "$1" "Exif Image Height"`
 	BRENNWEITE=`imageTag "$1" "Focal Length"`
 	BLENDE=`imageTag "$1" "F-Number"`
 	EXPTIME=`imageTag "$1" "Exposure Time"`
 	DATETIME=`imageTag "$1" "Date and Time \(Origi" | sed 's/:/-/' | sed 's/:/-/'`
 	PROGRAM=`imageTag "$1" "Exposure Program"`
-	RESOLUTION_X=`imageTag "$1" "X-Resolution"`
-	RESOLUTION_Y=`imageTag "$1" "Y-Resolution"`
-	WIDTH=`imageTag "$1" "Image Width"`
-	LENGTH=`imageTag "$1" "Image Length"`
+	RESOLUTION_X=`imageTag "$1" "X Resolution"`
+	RESOLUTION_Y=`imageTag "$1" "Y Resolution"`
 	META=`imageTag "$1" "Image Description"`
 	FOTOGRAPH=`imageTag "$1" "Artist"`
 	TIMSTMP="`date +'%Y-%m-%d %H:%M:%S'`"
@@ -34,7 +32,7 @@ processImage() {
 	CONCERTNAME=`basename "$DIRECTORY" | awk -F '_' '{print $2}'`
 
 	echo "INSERT INTO Concert (date,title) SELECT '$CONCERTDATE', '$CONCERTNAME' WHERE NOT EXISTS (SELECT * FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME');"
-	echo "INSERT INTO ConcertPicture (concertId,dim_X,dim_Y,brennweite,blende,verschlusszeit,insertdate,eventdate,program,resolution_X,resolution_Y,width,length,meta,fotograph,thumbnail,filename) SELECT id,'$DIM_X','$DIM_Y','$BRENNWEITE','$BLENDE','$EXPTIME','$TIMSTMP','$DATETIME','$PROGRAM','$RESOLUTION_X','$RESOLUTION_Y','$WIDTH','$LENGTH','$META','$FOTOGRAPH','$THUMBNAIL','$FILENAME' FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME';"
+	echo "INSERT INTO ConcertPicture (concertId,focaldist,apperture,shutterspeed,insertdate,eventdate,program,resolution_X,resolution_Y,width,length,meta,fotographer,thumbnail,filename) SELECT id,'$BRENNWEITE','$BLENDE','$EXPTIME','$TIMSTMP','$DATETIME','$PROGRAM','$RESOLUTION_X','$RESOLUTION_Y','$WIDTH','$LENGTH','$META','$FOTOGRAPH','$THUMBNAIL','$FILENAME' FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME';"
 }
 
 DATADIR=/data/ellokal.project-strus.net/
