421d18d39340680742fdc3a62ed2f7851a65e732
patrickfrey <patrickpfrey@yahoo.com>
2015-10-23 17:51:57 +0200

    ...

--- scripts/insertPictures.sh
+++ scripts/insertPictures.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 imageTag() {
-	exiftool "$1" | awk -F ':' '{gsub(/[ \t]+$/, "", $1); gsub(/^[ \t]+/, "", $2); print $1 "=" $2}' | grep "$1=" | awk -F'=' "/[=]/"'{print $2}' | head -n 1
+	exiftool -$2 "$1" | awk -F':' '{print $2}'
 }
 
 thumbnail() {
@@ -14,17 +14,17 @@ thumbnail() {
 processImage() {
 	FILENAME=`echo "$1" | sed 's@/data/ellokal.project-strus.net/@@'`
 	THUMBNAIL=`thumbnail "$1"`
-	WIDTH=`imageTag "$1" "Exif Image Width"`
-	LENGTH=`imageTag "$1" "Exif Image Height"`
-	BRENNWEITE=`imageTag "$1" "Focal Length"`
-	BLENDE=`imageTag "$1" "F-Number"`
-	EXPTIME=`imageTag "$1" "Exposure Time"`
-	DATETIME=`imageTag "$1" "Date and Time \(Origi" | sed 's/:/-/' | sed 's/:/-/'`
-	PROGRAM=`imageTag "$1" "Exposure Program"`
-	RESOLUTION_X=`imageTag "$1" "X Resolution"`
-	RESOLUTION_Y=`imageTag "$1" "Y Resolution"`
-	META=`imageTag "$1" "Image Description"`
-	FOTOGRAPH=`imageTag "$1" "Artist"`
+	WIDTH=`exiftool -ImageWidth "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	LENGTH=`exiftool -ImageHeight "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	BRENNWEITE=`exiftool -FocalLength "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	BLENDE=`exiftool -FNumber "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	EXPTIME=`exiftool -ExposureTime "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	DATETIME=`exiftool -DateTimeOriginal "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	PROGRAM=`exiftool -ExposureProgram "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	RESOLUTION_X=`exiftool -XResolution "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	RESOLUTION_Y=`exiftool -YResolution "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	META=`exiftool -ImageDescription "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
+	FOTOGRAPHER=`exiftool -Artist "$1" | awk '{sub(/:/,"~")}1' | awk -F'~'{print $2}`
 	TIMSTMP="`date +'%Y-%m-%d %H:%M:%S'`"
 
 	DIRECTORY="`dirname 'data/2014.08.25_howe gelb/D800E_040396.jpg'`"
@@ -43,13 +43,13 @@ processImage() {
 	echo "RESOLUTION_X='$RESOLUTION_X'";
 	echo "RESOLUTION_Y='$RESOLUTION_Y'";
 	echo "META='$META'";
-	echo "FOTOGRAPH='$FOTOGRAPH'";
+	echo "FOTOGRAPHER='$FOTOGRAPHER'";
 	echo "TIMSTMP='$TIMSTMP'";
 	echo "CONCERTDATE='$CONCERTDATE'";
 	echo "CONCERTNAME='$CONCERTNAME'";
 
 	echo "INSERT INTO Concert (date,title) SELECT '$CONCERTDATE', '$CONCERTNAME' WHERE NOT EXISTS (SELECT * FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME');"
-	echo "INSERT INTO ConcertPicture (concertId,focaldist,apperture,shutterspeed,insertdate,eventdate,program,resolution_X,resolution_Y,width,length,meta,fotographer,thumbnail,filename) SELECT id,'$BRENNWEITE','$BLENDE','$EXPTIME','$TIMSTMP','$DATETIME','$PROGRAM','$RESOLUTION_X','$RESOLUTION_Y','$WIDTH','$LENGTH','$META','$FOTOGRAPH','$THUMBNAIL','$FILENAME' FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME';"
+	echo "INSERT INTO ConcertPicture (concertId,focaldist,apperture,shutterspeed,insertdate,eventdate,program,resolution_X,resolution_Y,width,length,meta,fotographer,thumbnail,filename) SELECT id,'$BRENNWEITE','$BLENDE','$EXPTIME','$TIMSTMP','$DATETIME','$PROGRAM','$RESOLUTION_X','$RESOLUTION_Y','$WIDTH','$LENGTH','$META','$FOTOGRAPHER','$THUMBNAIL','$FILENAME' FROM Concert WHERE date='$CONCERTDATE' AND title='$CONCERTNAME';"
 }
 
 DATADIR=/data/ellokal.project-strus.net/
